c22d360730e66cd8385c35cdd2d1a18da181f393
tak <wontaek22@gmail.com>
2023-04-12 22:18:09 +0900

    23.04.12
    백준 10798번 문제 (2차원 배열 : 세로읽기)

---
+++ src/com/tak/study/Main.java
@@ -0,0 +1,21 @@
+package com.tak.study;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //값을 담는 Str[] 선언..
+        String[] str = new String[5];
+
+
+
+        //아... 요즘 일도 너무 많고 피곤해서.. 도저히 머리를 못쓰겠다...
+
+
+    }
+
+}
